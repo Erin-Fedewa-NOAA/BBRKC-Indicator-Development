68b5ec1335aecca559dd65465520fa95c2c89c18
Erin.Fedewa <Erin.Fedewa@nmfs.local>
2025-09-03 15:24:48 -0800

    Clutch failure exploration

--- Scripts/BBRKC_Clutch_Fullness.R
+++ Scripts/BBRKC_Clutch_Fullness.R
@@ -3,10 +3,28 @@
 #Author: Erin Fedewa
 
 #NOTE: Resampling wasn't started until 1999 so in an attempt to standardize, we're
-  #using newshell mature females only, and not replacing with rasampling data- though
-  #this metric is still tricky to interpret 
+  #using newshell mature females only, and not replacing with rasampling data. A few
+  #things to follow up on here:
 
-#2026 to due: Overlay with Kodiak RKC clutch failure data 
+#1) Look at proportion of shell condition 1 females that have clutches- might be worth 
+  #including them since we know they've molted/mated?
+#2) Look at the proportion of the mature female population this actually includes-
+  #b/c probably a very small portion in resampling yrs, and we're assuming this metric 
+  #is representative of the population - would be good to caveat results with this
+#3) We should be taking this time series back to 1980 because this seems to be an informative
+  #year for assigning an upper threshold (i.e. ~40% clutch failure during collapse!) but it 
+  #would be worth digging into 1980 data a bit more to make sure this is a real signal- i.e. were
+  #these all dead eggs, were most mature females old shell, skewed sex ratio etc?
+#4) Also a note that pre-1980, there are many very small females classified as mature, and this
+  #results in very high %empty, BUT these females are not likely mature. Safer bet to use 
+  #1980+, and there are ongoing SAP conversations about retrospectively correcting some of 
+  #these early data. OR you could use earlier years, but use GE90mm subset of females, which
+  #is the ADFG cutline 
+#5) Resampling data probably isn't much help here. Since the protocol wasn't started until 1999, 
+  #replacing resampling data would mean pre-1999 data aren't really comparable 
+
+#2026 to do: Overlay with Kodiak RKC clutch failure data? I think this is Resolution data but need 
+  #to confirm that clutch codes are classified using the same methods 
 
 library(tidyverse)
 library(ggridges)
